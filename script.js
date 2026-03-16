0ce85b42a07ac1d58daefedb9b9f7fcb097912b9
Guba <gustavopessoa211@gmail.com>
2026-03-16 16:41:41 -0700

    Segundo tabuleiro no meu jogo de batalha naval.

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-const letras = ['A','B','C','D','E','F','G','H','I','J'];
-const containerX = document.getElementById('x');
-const containerY = document.getElementById('y');
-const tabuleiro = document.getElementById('tabuleiro');
+const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
-letras.forEach(l => {
-    const div = document.createElement('div');
-    div.className = 'letra';
-    div.innerText = l;
-    containerX.appendChild(div);
-});
+function criarTabuleiro(x, y, tabuleiro) {
 
-for (let i = 1; i <= 10; i++) {
-    const div = document.createElement('div');
-    div.className = 'numero';
-    div.innerText = i;
-    containerY.appendChild(div);
+    for (let i = 0; i < 10; i++) {
+
+        let div = document.createElement("div")
+        div.className = "letra"
+        div.innerText = letras[i]
+        x.appendChild(div)
+
+    }
+
+    for (let i = 1; i <= 10; i++) {
+
+        let div = document.createElement("div")
+        div.className = "numero"
+        div.innerText = i
+        y.appendChild(div)
+
+    }
+
+    for (let i = 0; i < 100; i++) {
+
+        let q = document.createElement("div")
+        q.className = "quadrado"
+        tabuleiro.appendChild(q)
+
+    }
+
 }
 
-for (let i = 0; i < 100; i++) {
-    const quad = document.createElement('div');
-    quad.className = 'quadrado';
-    quad.dataset.row = Math.floor(i / 10);
-    quad.dataset.col = i % 10;
-    tabuleiro.appendChild(quad);
-}
+criarTabuleiro(
+    document.getElementById("x1"),
+    document.getElementById("y1"),
+    document.getElementById("tabuleiro1")
+)
+
+criarTabuleiro(
+    document.getElementById("x2"),
+    document.getElementById("y2"),
+    document.getElementById("tabuleiro2")
+)
